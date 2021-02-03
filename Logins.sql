948446c42a94f571eb7b2d0b724c6024a14c2bf8
Incógnita X <julian.cortes43@gmail.com>
2021-02-03 17:02:37 -0500

    Update Logins.sql

--- Logins.sql
+++ Logins.sql
@@ -1,1 +1,15 @@
+--Login 
+
+CREATE LOGIN Marley Lemke WITH PASSWORD = 'AYilQsT2ZEz:)u'
+GO
+--El usuario cambia la contraseña en el proximo inicio reinicio y no caduca la contraseña
+CREATE LOGIN Emile Luettgen WITH PASSWORD = 'ZR#ebOL.D{#/Mj;' MUST_CHANGE,
+CHECK_EXPIRATION = ON;
+GO
+
+--No expira la contraseña y se le aplcia politica a la contraseña
+CREATE LOGIN Charlotte Reynolds WITH PASSWORD = 'Jp%AjZTVrr'
+CHECK_EXPIRATION = ON,
+CHECK_POLICY = ON;
+GO
 
